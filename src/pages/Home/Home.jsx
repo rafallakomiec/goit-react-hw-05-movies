987d0b8e0d29f94ffff52a743dd89663999fc78f
Rafal Lakomiec <rafal.lakomiec@gmail.com>
2024-03-13 20:04:08 +0100

    create page Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchTrending } from '../../utils/fetchMovies';
+import css from './Home.module.css';
+
+const Home = () => {
+  const { isLoading, setIsLoading } = useState(true);
+  const { top20, setTop20 } = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const responseData = await fetchTrending();
+      const list = responseData.results.map(movie => {
+        return (
+          <li key={movie.id}>
+            <Link to={'/movies' + movie.id}>{movie.title}</Link>
+          </li>
+        );
+      });
+      setTop20(list);
+      setIsLoading(false);
+    })();
+  });
+
+  return (
+    <>
+      {isLoading && (
+        <div className={css.fallback}>
+          Loading content...
+          <br />
+          Please wait.
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <h1 className={css.subtitle}>Trending today</h1>
+          <ul className={css.list}>{top20}</ul>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Home;
