d51ecfc38eac2bea508b66e1d661b4ca30b717f4
Rafal Lakomiec <rafal.lakomiec@gmail.com>
2024-03-13 19:07:09 +0100

    create fetch trending util

---
+++ src/utils/fetchMovies.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const options = {
+  baseURL: 'https://api.themoviedb.org/',
+  headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTgwNzg4ODVmNTI4MGFhZTliOGExMjc4NGVjOWRlMyIsInN1YiI6IjY1ZjBiMWYwM2RjODg1MDE4NWM1OGMxMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xPEH3lPfpJB3gniIsVuxoAPMdf5HkmnVHtEgcGtmmOY'
+  }
+};
+
+const fetchTrending = async () => { 
+    try {
+      const response = await axios('3/trending/movie/day?language=en-US', options);
+      if (response.status !== 200) throw new Error(response.statusText);
+      return response.data;
+    } catch (error) {
+      return (<h2>Error occured: {error}.<br/>Please reload the page.</h2>);
+    }
+};
+
+export {fetchTrending};
