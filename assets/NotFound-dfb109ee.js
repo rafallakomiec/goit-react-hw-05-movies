917f709d12e4e0ea682226fc491b340166b210da
Rafal Lakomiec <rafal.lakomiec@gmail.com>
2024-03-14 21:49:21 +0100

    Updates

---
+++ assets/NotFound-dfb109ee.js
@@ -0,0 +1,1 @@
+import{j as o,L as t}from"./index-a1982b94.js";const e=()=>o.jsxs(o.Fragment,{children:[o.jsx("h2",{children:"You seem to got lost..."}),o.jsx(t,{to:"/",children:"Go back Home"})]});export{e as default};
